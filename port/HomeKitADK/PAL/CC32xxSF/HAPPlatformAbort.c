8f8915698b8e734c1e8039e49623fe35376ab774
jbuonagurio <jbuonagurio@exponent.com>
2021-11-29 17:50:25 -0500

    Improve abort handling

--- port/HomeKitADK/PAL/CC32xxSF/HAPPlatformAbort.c
+++ port/HomeKitADK/PAL/CC32xxSF/HAPPlatformAbort.c
@@ -10,7 +10,25 @@
 
 #include "HAPPlatform.h"
 
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <ti/devices/cc32xx/inc/hw_types.h>
+#include <ti/devices/cc32xx/driverlib/prcm.h>
+#include <ti/devices/cc32xx/driverlib/rom_map.h>
+#include <ti/drivers/net/wifi/simplelink.h>
+
 HAP_NORETURN
-void HAPPlatformAbort(void) {
+void HAPPlatformAbort(void)
+{
+    // Stop the NWP.
+    sl_Stop(200);
+
+    // Short delay for debug log output before reset.
+    vTaskDelay(pdMS_TO_TICKS(500));
+
+    // Trigger a hibernate cycle for the device using RTC.
+    MAP_PRCMHibernateCycleTrigger();
+
     while (1) {}
 }
