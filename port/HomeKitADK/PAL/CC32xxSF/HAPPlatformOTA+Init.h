b2c6cf659c6a637aea9265ea0b70bacd57537a80
jbuonagurio <jbuonagurio@exponent.com>
2022-01-04 01:55:40 -0500

    Add initial OTA implementation based on FreeRTOS.

---
+++ port/HomeKitADK/PAL/CC32xxSF/HAPPlatformOTA+Init.h
@@ -0,0 +1,160 @@
+/*
+ * FreeRTOS OTA PAL for CC3220SF-LAUNCHXL V2.0.0
+ * Copyright (c) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * Copyright (c) 2021 John Buonagurio
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
+ */
+
+#ifndef HAP_PLATFORM_OTA_INIT_H
+#define HAP_PLATFORM_OTA_INIT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "HAPPlatform.h"
+
+#if __has_feature(nullability)
+#pragma clang assume_nonnull begin
+#endif
+
+/**
+ * OTA image state.
+ */
+HAP_ENUM_BEGIN(uint8_t, HAPPlatformOTAImageState) {
+    kHAPPlatformOTAImageState_Unknown,  /*!< @brief The initial state of the OTA MCU Image. */
+    kHAPPlatformOTAImageState_Testing,  /*!< @brief The state of the OTA MCU Image post successful download and reboot. */
+    kHAPPlatformOTAImageState_Accepted, /*!< @brief The state of the OTA MCU Image post successful download and successful self test. */
+    kHAPPlatformOTAImageState_Rejected, /*!< @brief The state of the OTA MCU Image when the job has been rejected. */
+    kHAPPlatformOTAImageState_Aborted   /*!< @brief The state of the OTA MCU Image after a timeout publish to the stream request fails. */
+} HAP_ENUM_END(uint8_t, HAPPlatformOTAImageState);
+
+/**
+ * OTA PAL image state.
+ */
+HAP_ENUM_BEGIN(uint8_t, HAPPlatformOTAPALImageState) {
+    kHAPPlatformOTAPALImageState_Unknown,       /*!< @brief The initial state of the OTA PAL Image. */
+    kHAPPlatformOTAPALImageState_PendingCommit, /*!< @brief OTA PAL Image awaiting update. */
+    kHAPPlatformOTAPALImageState_Valid,         /*!< @brief OTA PAL Image is valid. */
+    kHAPPlatformOTAPALImageState_Invalid        /*!< @brief OTA PAL Image is invalid. */
+} HAP_ENUM_END(uint8_t, HAPPlatformOTAPALImageState);
+
+/**
+ * OTA file context.
+ */
+typedef struct {
+    uint8_t *filePath;           /*!< @brief Update file pathname. */
+    int32_t file;                /*!< @brief File descriptor. */
+    uint32_t fileSize;           /*!< @brief The size of the file in bytes. */
+    uint8_t *certFilePath;       /*!< @brief Pathname of the certificate file used to validate the receive file. */
+    uint32_t fileType;           /*!< @brief The file type id set when creating the OTA job. */
+    uint8_t *signature;          /*!< @brief Pointer to the file's signature. */
+    uint32_t signatureSize;      /*!< @brief The size of the file's signature in bytes. */
+} HAPPlatformOTAContext;
+
+/**
+ * Create a new receive file for the data chunks as they come in.
+ *
+ * @param[in]  otaContext           OTA file context information.
+ *
+ * @return kHAPError_None           If successful.
+ * @return kHAPError_OutOfResources If the file to be created exceeds the device's non-volatile memory size constraints.
+ * @return kHAPError_Unknown        Other errors creating the file in the device's non-volatile memory.
+ */
+HAP_RESULT_USE_CHECK
+HAPError HAPPlatformOTACreate(HAPPlatformOTAContext *otaContext);
+
+/**
+ * Authenticate and close the underlying receive file in the specified OTA context.
+ * 
+ * @param[in] otaContext            OTA file context information.
+ * 
+ * @return kHAPError_None           If successful.
+ * @return kHAPError_NotAuthorized  If cryptographic signature verification fails.
+ * @return kHAPError_Unknown        Other errors.
+ */
+HAP_RESULT_USE_CHECK
+HAPError HAPPlatformOTAClose(const HAPPlatformOTAContext *otaContext);
+
+/**
+ * Write a block of data to the specified file at the given offset.
+ *
+ * @param[in] otaContext            OTA file context information.
+ * @param[in] offset                Byte offset to write to from the beginning of the file.
+ * @param[in] bytes                 Pointer to the byte array of data to write.
+ * @param[in] numBytes              The number of bytes to write.
+ *
+ * @return The number of bytes written on success, or a negative error code.
+ */
+int16_t HAPPlatformOTAWriteBlock(const HAPPlatformOTAContext *otaContext,
+                                 uint32_t offset,
+                                 uint8_t *bytes,
+                                 uint32_t numBytes);
+
+/**
+ * Activate the newest MCU image received via OTA. This function should not return.
+ *
+ * @return kHAPError_None           If successful.
+ * @return kHAPError_Unknown        If activation failed.
+ */
+HAPError HAPPlatformOTAActivateNewImage(const HAPPlatformOTAContext *otaContext);
+
+/**
+ * Reset the device. This function should not return.
+ * 
+ * @return kHAPError_None           If successful.
+ * @return kHAPError_Unknown        If reset failed.
+ */
+HAPError HAPPlatformOTAResetDevice(void);
+
+/**
+ * Attempt to set the state of the OTA update image.
+ *
+ * @param[in] otaContext            File context of type OtaFileContext_t.
+ * @param[in] state                 The desired state of the OTA update image.
+ *
+ * @return kHAPError_None           If successful.
+ * @return kHAPError_Unknown        If state could not be set.
+ */
+HAP_RESULT_USE_CHECK
+HAPError HAPPlatformOTASetImageState(const HAPPlatformOTAContext *otaContext,
+                                     HAPPlatformOTAImageState state);
+
+/**
+ * @brief Get the state of the OTA update image.
+ *
+ * @param[in] otaContext            File context of type OtaFileContext_t.
+ *
+ * @return A value of type HAPPlatformOTAPALImageState.
+ */
+HAP_RESULT_USE_CHECK
+HAPPlatformOTAPALImageState HAPPlatformOTAGetImageState(const HAPPlatformOTAContext *otaContext);
+
+#if __has_feature(nullability)
+#pragma clang assume_nonnull end
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ port/HomeKitADK/PAL/CC32xxSF/HAPPlatformOTA.c
@@ -0,0 +1,386 @@
+/*
+ * FreeRTOS OTA PAL for CC3220SF-LAUNCHXL V2.0.0
+ * Copyright (c) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * Copyright (c) 2021 John Buonagurio
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
+ */
+
+#include <stdint.h>
+
+#include <ti/drivers/net/wifi/simplelink.h>
+#include <ti/devices/cc32xx/driverlib/rom_map.h>
+#include <ti/devices/cc32xx/inc/hw_types.h>
+#include <ti/devices/cc32xx/driverlib/prcm.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "HAPPlatformOTA+Init.h"
+
+static const HAPLogObject logObject = { .subsystem = kHAPPlatform_LogSubsystem, .category = "OTA" };
+
+/* This is our specific file token for OTA updates. */
+#define kOTA_VendorToken 1952007250UL
+
+/* Maximum allowed size of an MCU update image for this platform. */
+#define kOTA_MaxImageSize (512UL * 1024UL)
+
+/* Maximum allowed block write retries (in addition to the first try). */
+#define kOTA_MaxBlockWriteRetries 3U
+
+/* Maximum allowed retries to create the OTA receive file. */
+#define kOTA_MaxCreateRetries 1
+
+/* Use a 16 second watchdog timer. /2 for 2x factor from system clock. */
+#define kOTA_WDTTimeout (16UL / 2UL)
+
+/* TI SimpleLink watchdog timer start key (APPS_WDT_START_KEY). */
+#define kOTA_WDTStartKey 0xAE42DB15UL
+
+/* The TI SimpleLink watchdog clock source runs at 80MHz. */
+#define kOTA_WDTClockHz 80000000UL
+
+typedef struct {
+    uint8_t ucActiveImg;
+    uint32_t ulImgStatus;
+    uint32_t ulStartWdtKey;
+    uint32_t ulStartWdtTime;
+} sBootInfo_t;
+
+static void RollbackBundle(void)
+{
+    SlFsControl_t fsControl;
+    fsControl.IncludeFilters = 0U;
+    int32_t retval = sl_FsCtl(SL_FS_CTL_BUNDLE_ROLLBACK,
+                              0UL,
+                              (uint8_t *)NULL,
+                              (uint8_t *)&fsControl,
+                              (uint16_t)sizeof(SlFsControl_t),
+                              (uint8_t *)NULL,
+                              0U,
+                              (uint32_t *)NULL);
+    if (retval != 0) {
+        HAPLogError(&logObject, "Bundle rollback failed (%ld).", retval);
+    }
+    else {
+        HAPLogInfo(&logObject, "Bundle rollback succeeded.");
+    }
+}
+
+static void RollbackRxFile(HAPPlatformOTAContext *otaContext)
+{
+    uint32_t newToken = 0UL; /* New token is not retained. */
+    SlFsControl_t fsControl;
+    fsControl.IncludeFilters = 0U;
+    int32_t retval = sl_FsCtl(SL_FS_CTL_ROLLBACK,
+                              kOTA_VendorToken,
+                              (uint8_t *)otaContext->filePath,
+                              (uint8_t *)&fsControl,
+                              (uint16_t)sizeof(SlFsControl_t),
+                              (uint8_t *)NULL,
+                              0U,
+                              (uint32_t *)&newToken);
+    if (retval != 0) {
+        HAPLogError(&logObject, "File %s rollback failed (%ld).", otaContext->filePath, retval);
+    }
+    else {
+        HAPLogInfo(&logObject, "File %s rolled back.", otaContext->filePath);
+    }
+}
+
+static int32_t CreateBootInfoFile(void)
+{
+    uint32_t token = kOTA_VendorToken;
+    int32_t retval;
+
+    int32_t fd = sl_FsOpen((const uint8_t *)"/sys/mcubootinfo.bin",
+                            SL_FS_CREATE | SL_FS_OVERWRITE | SL_FS_CREATE_MAX_SIZE(sizeof(sBootInfo_t)) |
+                            SL_FS_CREATE_SECURE | SL_FS_CREATE_VENDOR_TOKEN |
+                            SL_FS_CREATE_PUBLIC_WRITE | SL_FS_CREATE_NOSIGNATURE,
+                            (_u32 *)&token);
+
+    if (fd < 0) {
+        HAPLogError(&logObject, "Error opening bootinfo file: %ld.", fd);
+        /* Propagate error to caller. */
+        retval = fd;
+    }
+    else {
+        sBootInfo_t bootInfo;
+        HAPRawBufferZero(&bootInfo, sizeof(sBootInfo_t));
+        bootInfo.ulStartWdtTime = kOTA_WDTClockHz * kOTA_WDTTimeout;
+        bootInfo.ulStartWdtKey = kOTA_WDTStartKey;
+        retval = sl_FsWrite(fd, 0UL, (uint8_t *)&bootInfo, (uint32_t)sizeof(sBootInfo_t));
+        if (retval != (int32_t)sizeof(sBootInfo_t)) {
+            HAPLogError(&logObject, "Error writing bootinfo file: %ld.", retval);
+            if (retval > 0) {
+                /* Force a fail result to the caller. Map to zero bytes written. */
+                retval = 0;
+            }
+            /* Else retval is a negative error code. */
+        }
+
+        /* Close the file even after a write failure. */
+        int32_t closeResult = sl_FsClose(fd, (uint8_t *) NULL, (uint8_t *) NULL, 0UL);
+        if (closeResult < 0) {
+            HAPLogError(&logObject, "Error closing bootinfo file: %ld.", closeResult);
+            /* Return the most recent error code to the caller. */
+            retval = closeResult;
+        }
+    }
+
+    return retval;
+}
+
+HAPError HAPPlatformOTACreate(HAPPlatformOTAContext *otaContext)
+{
+    HAPError err = kHAPError_Unknown;
+    uint32_t token = kOTA_VendorToken;
+    uint32_t flags;
+    int32_t retval;
+
+    otaContext->file = -1;
+
+    if (otaContext->fileSize <= kOTA_MaxImageSize) {
+        if(otaContext->fileType == 0) {
+            flags = SL_FS_CREATE | SL_FS_OVERWRITE | SL_FS_CREATE_FAILSAFE |
+                    SL_FS_CREATE_PUBLIC_WRITE | SL_FS_WRITE_BUNDLE_FILE |
+                    SL_FS_CREATE_SECURE | SL_FS_CREATE_VENDOR_TOKEN;
+            /* Create a boot info file for configuring watchdog timer. */
+            retval = CreateBootInfoFile();
+        }
+        else {
+            flags = SL_FS_CREATE | SL_FS_OVERWRITE | SL_FS_CREATE_NOSIGNATURE;
+            /* Set the retval explicitly as we do not create a boot info file for non firmware files. */
+            retval = 1;
+        }
+
+        /* CreateBootInfoFile returns the number of bytes written or negative error. 0 is not allowed. */
+        if (retval > 0) {
+            int32_t retry = 0;
+            do {
+                /* The file remains open until the OTA agent calls HAPPlatformOTAClose() after transfer or failure. */
+                retval = sl_FsOpen((uint8_t *)otaContext->filePath, (uint32_t)(flags | SL_FS_CREATE_MAX_SIZE(otaContext->fileSize)), (uint32_t *)&token);
+                if (retval > 0) {
+                    HAPLogInfo(&logObject, "Receive file created. Token: %lu.", token);
+                    otaContext->file = (int32_t)retval;
+                    err = kHAPError_None;
+                }
+                else {
+                    HAPLogError(&logObject, "Error (%ld) trying to create receive file.", retval);
+                    if (retval == SL_ERROR_FS_FILE_IS_ALREADY_OPENED) {
+                        /* System is in an inconsistent state and must be rebooted. */
+                        if (HAPPlatformOTAResetDevice() != kHAPError_None) {
+                            HAPLogError(&logObject, "Failed to reset the device via software.");
+                        }
+                    }
+                    else if (retval == SL_ERROR_FS_FILE_IS_PENDING_COMMIT) {
+                        /* Attempt to roll back the receive file and try again. */
+                        RollbackRxFile(otaContext);
+                    }
+                    else {
+                        if (otaContext->fileType == 0) {
+                            /* Attempt to roll back the bundle and try again. */
+                            RollbackBundle();
+                        }
+                    }
+                    retry++;
+                    err = kHAPError_Unknown;
+                }
+            } while ((err != kHAPError_None) && (retry <= (int32_t)kOTA_MaxCreateRetries));
+        }
+        else {
+            /* Failed to create bootinfo file. */
+            err = kHAPError_Unknown;
+        }
+    }
+    else {
+        /* File is too big for the platform. */
+        HAPLogError(&logObject, "Error (%ld) trying to create receive file.", SL_ERROR_FS_FILE_MAX_SIZE_EXCEEDED);
+        err = kHAPError_OutOfResources;
+    }
+
+    return err;
+}
+
+HAPError HAPPlatformOTAClose(const HAPPlatformOTAContext *otaContext)
+{
+    HAPLogInfo(&logObject, "Authenticating and closing file.");
+    int16_t retval = sl_FsClose(otaContext->file,
+                                otaContext->certFilePath,
+                                otaContext->signature,
+                                (uint32_t)(otaContext->signatureSize));
+    switch (retval) {
+    case 0L:
+        return kHAPError_None;
+    case SL_ERROR_FS_WRONG_SIGNATURE_SECURITY_ALERT:
+    case SL_ERROR_FS_WRONG_SIGNATURE_OR_CERTIFIC_NAME_LENGTH:
+    case SL_ERROR_FS_CERT_IN_THE_CHAIN_REVOKED_SECURITY_ALERT:
+    case SL_ERROR_FS_INIT_CERTIFICATE_STORE:
+    case SL_ERROR_FS_ROOT_CA_IS_UNKOWN:
+    case SL_ERROR_FS_CERT_CHAIN_ERROR_SECURITY_ALERT:
+    case SL_ERROR_FS_FILE_NOT_EXISTS:
+    case SL_ERROR_FS_ILLEGAL_SIGNATURE:
+    case SL_ERROR_FS_WRONG_CERTIFICATE_FILE_NAME:
+    case SL_ERROR_FS_NO_CERTIFICATE_STORE:
+        return kHAPError_NotAuthorized;
+    default:
+        return kHAPError_Unknown;
+    }
+}
+
+int16_t HAPPlatformOTAWriteBlock(const HAPPlatformOTAContext *otaContext,
+                                 uint32_t offset,
+                                 uint8_t *bytes,
+                                 uint32_t numBytes)
+{
+    int32_t retval;
+    uint32_t numBytesWritten = 0;
+    uint32_t retry;
+    for (retry = 0UL; retry <= kOTA_MaxBlockWriteRetries; retry++) {
+        retval = sl_FsWrite(otaContext->file, offset + numBytesWritten, &bytes[numBytesWritten], numBytes);
+        if (retval >= 0) {
+            if (numBytes == (uint32_t)retval) {
+                /* If we wrote all of the bytes requested, we're done. */
+                break;
+            }
+            else {
+                numBytesWritten += (uint32_t)retval; /* Add to total bytes written counter. */
+                numBytes -= (uint32_t)retval; /* Reduce block size by amount just written. */
+            }
+        }
+    }
+
+    if (retry > kOTA_MaxBlockWriteRetries) {
+        HAPLogError(&logObject, "Aborted after %u retries.", kOTA_MaxBlockWriteRetries);
+        return (int16_t)SL_ERROR_FS_FAILED_TO_WRITE;
+    }
+    
+    return (int16_t)retval;
+}
+
+HAPError HAPPlatformOTAActivateNewImage(const HAPPlatformOTAContext *otaContext HAP_UNUSED)
+{
+    HAPLogInfo(&logObject, "Activating the new MCU image.");
+    return HAPPlatformOTAResetDevice();
+}
+
+HAPError HAPPlatformOTAResetDevice(void)
+{
+    HAPLogInfo(&logObject, "Stopping NWP and resetting the device.");
+
+    /* Stop the NWP. This will activate the bundle. Otherwise, we'll get a commit error later. */
+    sl_Stop(200U);
+
+    /* Short delay for debug log output before reset. */
+    vTaskDelay(pdMS_TO_TICKS(500UL));
+    MAP_PRCMHibernateCycleTrigger();
+
+    /* We shouldn't actually get here if the board supports the auto reset.
+     * But, it doesn't hurt anything if we do although someone will need to
+     * reset the device for the new image to boot. */
+    return kHAPError_Unknown;
+}
+
+HAPError HAPPlatformOTASetImageState(const HAPPlatformOTAContext *otaContext HAP_UNUSED,
+                                     HAPPlatformOTAImageState state)
+{
+    if (state == kHAPPlatformOTAImageState_Accepted) {
+        PRCMPeripheralReset((uint32_t)PRCM_WDT);
+        SlFsControl_t fsControl;
+        fsControl.IncludeFilters = 0U;
+        int32_t rc = sl_FsCtl(SL_FS_CTL_BUNDLE_COMMIT, (uint32_t)0, (uint8_t *)NULL, (uint8_t *)&fsControl, (uint16_t)sizeof(SlFsControl_t), (uint8_t *) NULL, (uint16_t)0, (uint32_t *)NULL);
+        if (rc != 0) {
+            HAPLogError(&logObject, "Accepted final image but commit failed (%ld).", rc);
+            return kHAPError_Unknown;
+        }
+        else {
+            HAPLogInfo(&logObject, "Accepted and committed final image.");
+            return kHAPError_None;
+        }
+    }
+    else if (state == kHAPPlatformOTAImageState_Rejected) {
+        SlFsControl_t fsControl;
+        fsControl.IncludeFilters = 0U;
+        int32_t rc = sl_FsCtl(SL_FS_CTL_BUNDLE_ROLLBACK, (uint32_t)0, (uint8_t *)NULL, (uint8_t *)&fsControl, (uint16_t)sizeof(SlFsControl_t), (uint8_t *) NULL, (uint16_t)0, (uint32_t *)NULL);
+        if (rc != 0) {
+            HAPLogError(&logObject, "Bundle rollback failed after reject (%ld).", rc);
+            return kHAPError_Unknown;
+        }
+        else {
+            HAPLogInfo(&logObject, "Image was rejected and bundle files rolled back.");
+            return kHAPError_None;
+        }
+    }
+    else if (state == kHAPPlatformOTAImageState_Aborted) {
+        SlFsControl_t fsControl;
+        fsControl.IncludeFilters = 0U;
+        int32_t rc = sl_FsCtl(SL_FS_CTL_BUNDLE_ROLLBACK, (uint32_t)0, (uint8_t *)NULL, (uint8_t *)&fsControl, (uint16_t)sizeof(SlFsControl_t), (uint8_t *) NULL, (uint16_t)0, (uint32_t *)NULL);
+        if (rc != 0) {
+            HAPLogError(&logObject, "Bundle rollback failed after abort (%ld).", rc);
+            return kHAPError_Unknown;
+        }
+        else {
+            HAPLogInfo(&logObject, "Agent aborted and bundle files rolled back.");
+            return kHAPError_None;
+        }
+    }
+    else if (state == kHAPPlatformOTAImageState_Testing) {
+        return kHAPError_None;
+    }
+    else {
+        HAPLogError(&logObject, "Unknown state received %ld.", (int32_t)state);
+        return kHAPError_Unknown;
+    }
+}
+
+HAPPlatformOTAPALImageState HAPPlatformOTAGetImageState(const HAPPlatformOTAContext *otaContext HAP_UNUSED)
+{
+    const uint32_t checkFlags = SL_FS_INFO_SYS_FILE | SL_FS_INFO_SECURE |
+                                SL_FS_INFO_NOSIGNATURE | SL_FS_INFO_PUBLIC_WRITE |
+                                SL_FS_INFO_PUBLIC_READ | SL_FS_INFO_NOT_VALID;
+
+    const uint32_t checkFlagsGood = SL_FS_INFO_SYS_FILE | SL_FS_INFO_SECURE | SL_FS_INFO_PUBLIC_WRITE;
+
+    HAPPlatformOTAPALImageState state = kHAPPlatformOTAPALImageState_Unknown;
+    SlFsFileInfo_t fileInfo;
+    int32_t rc = sl_FsGetInfo((const uint8_t *)"/sys/mcuflashimg.bin", kOTA_VendorToken, &fileInfo);
+    if (rc == 0) {
+        HAPLogDebug(&logObject, "Current platform image flags: %04x.", fileInfo.Flags);
+        if ((fileInfo.Flags & (uint16_t)SL_FS_INFO_PENDING_BUNDLE_COMMIT) != 0U) {
+            state = kHAPPlatformOTAPALImageState_PendingCommit;
+            HAPLogDebug(&logObject, "Current platform image state: kHAPPlatformOTAPALImageState_PendingCommit.");
+        }
+        else if ((fileInfo.Flags & checkFlags) == checkFlagsGood) {
+            state = kHAPPlatformOTAPALImageState_Valid;
+            HAPLogDebug(&logObject, "Current platform image state: kHAPPlatformOTAPALImageState_Valid.");
+        }
+        else {
+            state = kHAPPlatformOTAPALImageState_Invalid;
+            HAPLogDebug(&logObject, "Current platform image state: kHAPPlatformOTAPALImageState_Invalid.");
+        }
+    }
+    else {
+        HAPLogError(&logObject, "sl_FsGetInfo failed (%ld) on /sys/mcuflashimg.bin.", (int32_t)rc);
+        state = kHAPPlatformOTAPALImageState_Invalid;
+    }
+    return state;
+}
