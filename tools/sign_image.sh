6db0b084fa13fdb1a490e8f62ef77721c36082a6
jbuonagurio <jbuonagurio@exponent.com>
2022-01-28 23:25:03 -0500

    Add check for debug header to sign_image.sh.

--- tools/sign_image.sh
+++ tools/sign_image.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ] || [ "$4" == "" ] || [ $# != 4 ] ; then
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ] || [ "$4" == "" ] || [ $# != 4 ]; then
 	echo Usage: $0 \<input-image\> \<private-key\> \<output-image\> \<signature-file\>
 	exit 0
 fi
 
-echo preparing output image...
+if [[ `xxd -p -l 12 $1` == "5aa5a55a00f80f007d24a3ef" ]]; then
+	echo "$(basename $0): Image contains debug header." >&2
+	exit 1
+fi
+
+echo Preparing output image...
 openssl dgst -sha256 -binary $1 > $3
 cat $1 >> $3
-echo signing output image...
+
+echo Signing output image...
 openssl dgst -binary -sha256 -keyform DER -sign $2 -out $4 $3
+
 cat $4 | xxd -c 16 -i
