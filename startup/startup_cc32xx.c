08920fe8c86d8caafa89385d5d6a2054956737fa
jbuonagurio <jbuonagurio@exponent.com>
2022-02-21 02:34:01 -0500

    Move syscalls into separate file.

--- startup/startup_cc32xx.c
+++ startup/startup_cc32xx.c
@@ -32,7 +32,6 @@
 
 #include <stdint.h>
 #include <string.h>
-#include <reent.h>
 
 #include <ti/devices/cc32xx/inc/hw_nvic.h>
 #include <ti/devices/cc32xx/inc/hw_types.h>
@@ -237,64 +236,3 @@ __attribute__((noreturn)) void Default_Handler(void)
 {
     while(1) {}
 }
-
-/**
- * This function is called by __libc_fini_array which gets called when exit()
- * is called. In order to support exit(), an empty _fini() stub function is
- * required.
- */
-void _fini(void) {}
-
-/**
- * Implementation of _sbrk to support FreeRTOS with configUSE_NEWLIB_REENTRANT.
- */
-void * __attribute__((used)) _sbrk_r(struct _reent *r, int incr)
-{
-    extern char __heap_start__;
-    extern char __heap_end__;
-
-    static char *heap_start = &__heap_start__;
-
-    vTaskSuspendAll();
-
-    if (heap_start + incr > &__heap_end__)
-    {
-#if (configUSE_MALLOC_FAILED_HOOK == 1)
-        extern void vApplicationMallocFailedHook(void);
-        vApplicationMallocFailedHook();
-#endif
-        return (char *) - 1;
-    }
-
-    char *prev_heap_start = heap_start;
-    heap_start += incr;
-
-    xTaskResumeAll();
-
-    return prev_heap_start;
-}
-
-char * __attribute__((used))  sbrk(int incr) { return _sbrk_r(_impure_ptr, incr); }
-char * __attribute__((used)) _sbrk(int incr) { return _sbrk_r(_impure_ptr, incr); }
-
-void * __attribute__((used)) _malloc_r(struct _reent *r, size_t size)
-{
-    return pvPortMalloc(size);
-}
-
-void * __attribute__((used)) _calloc_r(struct _reent *r, size_t n, size_t size)
-{
-    void *p = pvPortMalloc(n * size);
-    if (p)
-        memset(p, 0, n * size);
-    return p;
-}
-
-void __attribute__((used)) _free_r(struct _reent *r, void *p)
-{
-    vPortFree(p);
-}
-
-void * __attribute__((used)) malloc(size_t size) { return _malloc_r(_impure_ptr, size); }
-void * __attribute__((used)) calloc(size_t n, size_t size) { return _calloc_r(_impure_ptr, n, size); }
-void __attribute__((used)) free(void *p) { _free_r(_impure_ptr, p); }
