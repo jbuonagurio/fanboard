a2884b39951a2a4646a7f49ea33a1a4263b3f904
jbuonagurio <jbuonagurio@exponent.com>
2022-02-28 02:08:51 -0500

    Remove soft reset code in HAPPlatformAbort.

--- port/HomeKitADK/PAL/CC32xxSF/HAPPlatformAbort.c
+++ port/HomeKitADK/PAL/CC32xxSF/HAPPlatformAbort.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-2019 The HomeKit ADK Contributors
 //
-// Copyright (c) 2020 John Buonagurio
+// Copyright (c) 2022 John Buonagurio
 // 
 // Licensed under the Apache License, Version 2.0 (the “License”);
 // you may not use this file except in compliance with the License.
@@ -25,15 +25,6 @@ void HAPPlatformAbort(void)
     // Set a breakpoint if debugger is connected (DHCSR[C_DEBUGEN] == 1).
     if ((*(volatile uint32_t *)NVIC_DBG_CTRL) & NVIC_DBG_CTRL_C_DEBUGEN)
         __asm("bkpt 1");
-
-    // Stop the NWP.
-    sl_Stop(200U);
-
-    // Short delay for debug log output before reset.
-    vTaskDelay(pdMS_TO_TICKS(500UL));
-
-    // Trigger a hibernate cycle for the device using RTC.
-    MAP_PRCMHibernateCycleTrigger();
 
     while (1) {}
 }
